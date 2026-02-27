61b7cc3feaefcf1cf063f350c87b9efd4fb53ead
Fei Kang <coffeykang@gmail.com>
2026-02-27 10:15:56 +0800

    change tel input type

--- resources/views/members/form.blade.php
+++ resources/views/members/form.blade.php
@@ -25,7 +25,7 @@
                             <label for="phone_number" class="block text-sm font-medium text-gray-700">{{ __('messages.enter_phone_number') }}</label>
                             <input
                                 id="phone_number"
-                                type="text"
+                                type="tel"
                                 wire:model.defer="phone_number"
                                 class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                                 placeholder="{{ __('messages.enter_phone_number') }}"
